70cc1cbbb9205bec396d74c9e2ce79a54424c729
Aum Patel <85500430+Patel-Aum-28@users.noreply.github.com>
2024-02-09 00:14:27 +0530

    -

--- client/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from client.models import EncryptedData
-
-# Register your models here.
-admin.site.register(EncryptedData)
--- client/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import payment_form
-
-app_name = 'client'
-
-urlpatterns = [
-    path('payment/', payment_form, name='payment_form'),
-]
